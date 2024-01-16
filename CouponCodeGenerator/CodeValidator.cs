940253996ed8605b0f058ea3107efe45a32317ff
ozandsahintas <ozan.deniz.sahintas@gmail.com>
2024-01-16 12:30:52 +0300

    Code Validator

---
+++ CouponCodeGenerator/CodeValidator.cs
@@ -0,0 +1,65 @@
+﻿using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CouponCodeGenerator
+{
+    public class CodeValidator
+    {
+        static readonly string dataSet = Configurator.CharacterSet;
+
+
+        public static string ValidateCoupon(int pass, string code, int gen)
+        {
+            Code? c = Validate(pass, code, gen);
+            if (c == null)
+            {
+                return "Coupon does not exist.";
+            }
+            else
+            {
+                return "Coupon " + c._code + " found, index: " + c._index;
+            }
+        }
+
+        static Code? Validate(int pass, string code, int gen)
+        {
+            var rnd = new Random(pass);
+            return ValidateCodes(rnd, code, gen);
+        }
+
+        static Code? ValidateCodes(Random r, string code, int gen, int length = 8)
+        {
+            if (code.Length < length) { return null; }
+
+            for (int i = 0; i < gen; i++)
+            {
+                string fullCode = "";
+                for (int j = 0; j < length; j++)
+                {
+                    char c = dataSet[r.Next(0, dataSet.Length)];
+                    fullCode += c;
+                }
+
+                if (string.Equals(code, fullCode))
+                {
+                    return new Code(code, i);
+                }
+
+            }
+            return null;
+        }
+
+    }
+
+    public class Code
+    {
+        public string _code;
+        public int _index;
+
+        public Code(string code, int index)
+        {
+            _code = code;
+            _index = index;
+        }
+    }
+}
--- CouponCodeGenerator/Program.cs
+++ CouponCodeGenerator/Program.cs
@@ -8,11 +8,30 @@ namespace CouponCodeGenerator
         {
             int generations = Configurator.Generations;
 
-            Console.WriteLine("Şifre:");
+            Console.WriteLine("Password:");
             int pass = Convert.ToInt32(Console.ReadLine());
 
-            var r = CodeGenerator.GenerateCoupons(pass, generations);
-            CodeGenerator.DisplayCoupons(pass, generations);
+            Console.WriteLine("Select an operation.");
+            Console.WriteLine("\n 1 - Generation \n 2 - Validation \n 0 - Exit ");
+            var selection = Convert.ToInt32(Console.ReadLine());
+
+            switch (selection)
+            {
+                case 0: Console.WriteLine(); break;
+                case 1:
+                    var r = CodeGenerator.GenerateCoupons(pass, generations);
+                    CodeGenerator.DisplayCoupons(pass, generations);
+                    break;
+                case 2:
+                    // pass: 555, code: T5YCRREE
+                    Console.WriteLine("Code:");
+                    string code = Console.ReadLine() ?? "";
+
+                    var coupon = CodeValidator.ValidateCoupon(pass, code, generations);
+                    Console.WriteLine(coupon);
+                    break;
+                default: Console.WriteLine("Wrong input."); break;
+            }
 
             Console.ReadLine();
         }
