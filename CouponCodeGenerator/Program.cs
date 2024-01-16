1cfd84779862c8cad99f0aae8185477b30005b8a
ozandsahintas <ozan.deniz.sahintas@gmail.com>
2024-01-16 05:33:36 +0300

    Initial
    with hello world :)

---
+++ CouponCodeGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CouponCodeGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
